6ac2d19a152f7acee67c0d759c3f5123c3a3d125
Parul kumawat <sspl.parul2026@gmail.com>
2026-07-06 18:41:45 +0530

    Implemented Room Controller

--- MeetingRoomBooking/MeetingRoomBooking/Controllers/RoomController.cs
+++ MeetingRoomBooking/MeetingRoomBooking/Controllers/RoomController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeetingRoomBooking.Models;
+using MeetingRoomBooking.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingRoomBooking.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Index()
+        private readonly IRoomService _roomService;
+        public RoomController(IRoomService roomService)
         {
-            return View();
+            _roomService = roomService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var rooms =  await _roomService.GetAllRooms();
+                
+                return View(rooms);
+            }catch(Exception)
+            {
+                ModelState.AddModelError("", "Unable to load meeting rooms");
+                return View(new List<Room>());
+            }
+           
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int roomId, DateOnly? date)
+        {
+            try
+            {
+                var room = await _roomService.GetRoomById(roomId);
+                if(room == null)
+                {
+                    return NotFound();
+                }
+                DateOnly selectedDate = date ?? DateOnly.FromDateTime(DateTime.Today);
+                var bookings = await _roomService.GetRoomBooking(roomId, selectedDate);
+                ViewBag.SelectedDate = selectedDate;
+                ViewBag.Bookings = bookings;
+                return View(room);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Unable to load meeting rooms");
+                return View();
+            }
         }
     }
 }
